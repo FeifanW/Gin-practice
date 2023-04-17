06dda9e038d70ee960e8c3f5c8cfacd2e0c32c9a
FeifanW <qihang791@163.com>
2023-04-17 10:51:17 +0800

    动态路由传值

--- gin_new/main.go
+++ gin_new/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/xml"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"html/template"
 	"net/http"
@@ -9,16 +11,26 @@ import (
 
 type H map[string]interface{}
 
-type Article struct {
-	Title   string `json:"title"`
-	Desc    string `json:"desc"`
-	Content string `json:"content"`
-}
+//type Article struct {
+//	Title   string `json:"title"`
+//	Desc    string `json:"desc"`
+//	Content string `json:"content"`
+//}
 
 // 时间戳转换成日期
 func UnixToTime(timestamp int) string {
 	t := time.Unix(int64(timestamp), 0)
 	return t.Format("2006-01-01 15:04:05")
+}
+
+type Userinfo struct {
+	Username string `form:"username" json:"user"`
+	Password string `form:"password" json:"password"`
+}
+
+type Article struct {
+	Title   string `form:"title" xml:"title"`
+	Content string `form:"content" xml:"content"`
 }
 
 func main() {
@@ -34,7 +46,15 @@ func main() {
 	r.Static("/static", "././static")
 	// 配置路由
 	r.GET("/", func(c *gin.Context) {
-		c.String(200, "值:%v", "你好gin")
+		username := c.Query("username")
+		age := c.Query("age")
+		page := c.DefaultQuery("page", "1")
+		c.JSON(http.StatusOK, gin.H{
+			"username": username,
+			"age":      age,
+			"page":     page,
+		})
+		//c.String(200, "值:%v", "你好gin")
 	})
 	r.GET("/news", func(c *gin.Context) {
 		c.String(200, "我是新闻页面")
@@ -51,44 +71,78 @@ func main() {
 			"msg":     "你好gin",
 		})
 	})
-	r.GET("/json2", func(c *gin.Context) {
-		a := &Article{
-			Title:   "我是一个标题",
-			Desc:    "描述",
-			Content: "测试内容",
+
+	// 获取GET POST 传递的数据绑定到结构体
+	r.GET("/getUser", func(c *gin.Context) {
+		user := &Userinfo{}
+		if err := c.ShouldBind(&user); err == nil {
+			fmt.Printf("%#v", user)
+			c.JSON(http.StatusOK, user)
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"err": err.Error(),
+			})
 		}
-		c.JSON(200, a)
 	})
-	// jsonp和json的区别就是可以执行回调函数
-	r.GET("/jsonp", func(c *gin.Context) {
-		a := &Article{
-			Title:   "我是一个jsonp",
-			Desc:    "描述",
-			Content: "测试内容",
-		}
-		c.JSONP(200, a)
-	})
+
+	//r.GET("/json2", func(c *gin.Context) {
+	//	a := &Article{
+	//		Title:   "我是一个标题",
+	//		Desc:    "描述",
+	//		Content: "测试内容",
+	//	}
+	//	c.JSON(200, a)
+	//})
+	//// jsonp和json的区别就是可以执行回调函数
+	//r.GET("/jsonp", func(c *gin.Context) {
+	//	a := &Article{
+	//		Title:   "我是一个jsonp",
+	//		Desc:    "描述",
+	//		Content: "测试内容",
+	//	}
+	//	c.JSONP(200, a)
+	//})
 	// 返回XML数据
-	r.GET("/xml", func(c *gin.Context) {
-		c.XML(http.StatusOK, gin.H{
-			"success": true,
-			"msg":     "你好gin，我是xml",
-		})
-	})
-	// 返回html
-	r.GET("/html", func(c *gin.Context) {
-		a := &Article{
-			Title:   "我是一个标题",
-			Desc:    "描述",
-			Content: "测试内容",
+	r.POST("/xml", func(c *gin.Context) {
+		article := &Article{}
+		xmlSliceData, _ := c.GetRawData() // 获取 c.Request.Body 读取请求数据
+		if err := xml.Unmarshal(xmlSliceData, &article); err == nil {
+			c.JSON(http.StatusOK, article)
+			fmt.Printf("%#v", article)
+		} else {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"err": err.Error(),
+			})
 		}
-		c.HTML(http.StatusOK, "test/index.html", gin.H{
-			"title": "我是后台的数据",
-			"news":  a,
-		})
 	})
+
+	// 动态路由传值
+	r.GET("/list/:cid", func(c *gin.Context) {
+		cid := c.Param("cid")
+		c.String(200, "%v", cid)
+	})
+
+	// 返回html
+	//r.GET("/html", func(c *gin.Context) {
+	//	a := &Article{
+	//		Title:   "我是一个标题",
+	//		Desc:    "描述",
+	//		Content: "测试内容",
+	//	}
+	//	c.HTML(http.StatusOK, "test/index.html", gin.H{
+	//		"title": "我是后台的数据",
+	//		"news":  a,
+	//	})
+	//})
 	r.POST("/add", func(c *gin.Context) {
-		c.String(200, "我是post请求返回的数据")
+		username := c.PostForm("username")
+		password := c.PostForm("password")
+		age := c.DefaultPostForm("age", "20")
+		c.JSON(http.StatusOK, gin.H{
+			"username": username,
+			"password": password,
+			"age":      age,
+		})
 	})
 	r.Run() // 启动web服务
 }
